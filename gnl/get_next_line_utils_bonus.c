0af19412eb0ba76c0eb1fd9f1a0fd6f2d08af062
Yejin Kim <yejikim@c8r1s10.42seoul.kr>
2021-11-23 19:32:00 +0900

    final gnl

--- gnl/get_next_line_utils_bonus.c
+++ gnl/get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 11:49:16 by yejin             #+#    #+#             */
-/*   Updated: 2021/11/23 16:19:08 by yejikim          ###   ########.fr       */
+/*   Updated: 2021/11/23 18:53:03 by yejikim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,50 @@ void	ft_strcpy(char *dst, char *src)
 		i++;
 	}
 	*(dst + i) = '\0';
+}
+
+int	ft_lstadd(t_list **lst, int fd)
+{
+	if (*lst != NULL)
+	{
+		if ((*lst)->curr >= (*lst)->last)
+		{
+			(*lst)->last = read(fd, (*lst)->buffer, BUFFER_SIZE);
+			if ((*lst)->last <= 0)
+				return (0);
+			(*lst)->curr = 0;
+		}
+		return (1);
+	}
+	*lst = (t_list *)malloc(sizeof(t_list));
+	if (*lst == NULL)
+		return (0);
+	(*lst)->next = NULL;
+	(*lst)->fd = fd;
+	(*lst)->buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if ((*lst)->buffer == NULL)
+		return (0);
+	(*lst)->last = read(fd, (*lst)->buffer, BUFFER_SIZE);
+	if ((*lst)->last <= 0)
+		return (0);
+	(*lst)->curr = 0;
+	return (1);
+}
+
+t_list	*ft_lstfind(t_list **lst, int fd)
+{
+	t_list	*curr;
+
+	curr = *lst;
+	while (curr)
+	{
+		if (curr->next == NULL)
+			return (curr);
+		else if (curr->next->fd == fd)
+			return (curr);
+		curr = curr->next;
+	}
+	return (curr);
 }
 
 void	free_element(t_list *temp)
