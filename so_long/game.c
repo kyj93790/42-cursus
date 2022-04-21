f1679f4373d7515c9e43e2e1ac9ba349248fab12
kyj93790 <kyj93790@naver.com>
2022-04-21 13:18:22 +0900

    map 정보에 따라 collectible print

--- so_long/game.c
+++ so_long/game.c
@@ -6,8 +6,12 @@ void	put_background_tiles(void *mlx_ptr, void *win_ptr, t_map *map_info)
 	t_ull	i;
 	t_ull	j;
 
-	tile.img_ptr = mlx_xpm_file_to_image(mlx_ptr, "tile.xpm", &(tile.width), &(tile.height));
-	// img 제대로 열리지 않는 경우 종료
+	tile.img_ptr = mlx_xpm_file_to_image(mlx_ptr, "imgs/tile.xpm", &(tile.width), &(tile.height));
+	if (tile.img_ptr == 0)
+	{
+		free_map(map_info);
+		exit_with_error("Failure in getting tile image");
+	}
 	i = 0;
 	while (i < map_info->height)
 	{
@@ -27,7 +31,7 @@ void put_wall(void *mlx_ptr, void *win_ptr, t_map *map_info)
 	t_ull	i;
 	t_ull	j;
 
-	bush.img_ptr = mlx_xpm_file_to_image(mlx_ptr, "bush.xpm", &(bush.width), &(bush.height));
+	bush.img_ptr = mlx_xpm_file_to_image(mlx_ptr, "imgs/bush.xpm", &(bush.width), &(bush.height));
 	if (bush.img_ptr == 0)
 	{
 		free_map(map_info);
@@ -41,6 +45,32 @@ void put_wall(void *mlx_ptr, void *win_ptr, t_map *map_info)
 		{
 			if (map_info->map[i][j] == '1')
 				mlx_put_image_to_window(mlx_ptr, win_ptr, bush.img_ptr, 64*j, 64*i);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	put_collectible(void *mlx_ptr, void *win_ptr, t_map *map_info)
+{
+	t_img pokeball;
+	t_ull i;
+	t_ull j;
+
+	pokeball.img_ptr = mlx_xpm_file_to_image(mlx_ptr, "imgs/pokeball.xpm", &(pokeball.width), &(pokeball.height));
+	if (pokeball.img_ptr == 0)
+	{
+		free_map(map_info);
+		exit_with_error("Failure in getting collectible image");
+	}
+	i = 0;
+	while (i < map_info->height)
+	{
+		j = 0;
+		while (j < map_info->width)
+		{
+			if (map_info->map[i][j] == 'C')
+				mlx_put_image_to_window(mlx_ptr, win_ptr, pokeball.img_ptr, 64*j, 64*i);
 			j++;
 		}
 		i++;
@@ -65,6 +95,7 @@ void	start_game(t_map *map_info)
 	}
 	put_background_tiles(mlx_ptr, win_ptr, map_info);
 	put_wall(mlx_ptr, win_ptr, map_info);
+	put_collectible(mlx_ptr, win_ptr, map_info);
 	//mlx_hook(win_ptr, X_EVENT_PRESS_KEY, 0, press_key, &loc);
 	mlx_loop(mlx_ptr);
 }
