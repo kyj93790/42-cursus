e67d9bd414e6170ba4ef7466e24174b5b7abafc3
kyj93790 <kyj93790@naver.com>
2022-06-04 02:06:43 +0900

    fix: 불필요한 Critical section 제거

--- Philosophers/philo/print_state.c
+++ Philosophers/philo/print_state.c
@@ -5,12 +5,10 @@ int	print_take_fork_state(t_philo *philo)
 	long			time_stamp;
 	struct timeval	curr_time;
 
-	pthread_mutex_lock(&(philo->monitor->m_print));
 	pthread_mutex_lock(&(philo->monitor->m_finish));
 	if (philo->monitor->finish_flag != 0)
 	{
 		pthread_mutex_unlock(&(philo->monitor->m_finish));
-		pthread_mutex_unlock(&(philo->monitor->m_print));
 		return (-1);
 	}
 	pthread_mutex_unlock(&(philo->monitor->m_finish));
@@ -19,10 +17,10 @@ int	print_take_fork_state(t_philo *philo)
 		pthread_mutex_lock(&(philo->monitor->m_finish));
 		philo->monitor->finish_flag = 2;
 		pthread_mutex_unlock(&(philo->monitor->m_finish));
-		pthread_mutex_unlock(&(philo->monitor->m_print));
 		return (-1);
 	}
 	time_stamp = calc_timeval(&(philo->monitor->start_time), &(curr_time));
+	pthread_mutex_lock(&(philo->monitor->m_print));
 	printf("%ldms\t%d\thas taken a fork\n", time_stamp, philo->id);
 	pthread_mutex_unlock(&(philo->monitor->m_print));
 	return (0);
@@ -33,12 +31,10 @@ int	print_eat_state(t_philo *philo)
 	long			time_stamp;
 	struct timeval	curr_time;
 
-	pthread_mutex_lock(&(philo->monitor->m_print));
 	pthread_mutex_lock(&(philo->monitor->m_finish));
 	if (philo->monitor->finish_flag != 0)
 	{
 		pthread_mutex_unlock(&(philo->monitor->m_finish));
-		pthread_mutex_unlock(&(philo->monitor->m_print));
 		return (-1);
 	}
 	pthread_mutex_unlock(&(philo->monitor->m_finish));
@@ -47,10 +43,10 @@ int	print_eat_state(t_philo *philo)
 		pthread_mutex_lock(&(philo->monitor->m_finish));
 		philo->monitor->finish_flag = 2;
 		pthread_mutex_unlock(&(philo->monitor->m_finish));
-		pthread_mutex_unlock(&(philo->monitor->m_print));
 		return (-1);
 	}
 	time_stamp = calc_timeval(&(philo->monitor->start_time), &(curr_time));
+	pthread_mutex_lock(&(philo->monitor->m_print));
 	printf("%ldms\t%d\tis eating\n", time_stamp, philo->id);
 	pthread_mutex_unlock(&(philo->monitor->m_print));
 	return (0);
@@ -61,12 +57,10 @@ int	print_sleep_state(t_philo *philo)
 	long			time_stamp;
 	struct timeval	curr_time;
 
-	pthread_mutex_lock(&(philo->monitor->m_print));
 	pthread_mutex_lock(&(philo->monitor->m_finish));
 	if (philo->monitor->finish_flag != 0)
 	{
 		pthread_mutex_unlock(&(philo->monitor->m_finish));
-		pthread_mutex_unlock(&(philo->monitor->m_print));
 		return (-1);
 	}
 	pthread_mutex_unlock(&(philo->monitor->m_finish));
@@ -75,10 +69,10 @@ int	print_sleep_state(t_philo *philo)
 		pthread_mutex_lock(&(philo->monitor->m_finish));
 		philo->monitor->finish_flag = 2;
 		pthread_mutex_unlock(&(philo->monitor->m_finish));
-		pthread_mutex_unlock(&(philo->monitor->m_print));
 		return (-1);
 	}
 	time_stamp = calc_timeval(&(philo->monitor->start_time), &(curr_time));
+	pthread_mutex_lock(&(philo->monitor->m_print));
 	printf("%ldms\t%d\tis sleeping\n", time_stamp, philo->id);
 	pthread_mutex_unlock(&(philo->monitor->m_print));
 	return (0);
@@ -89,12 +83,10 @@ int	print_think_state(t_philo *philo)
 	long			time_stamp;
 	struct timeval	curr_time;
 
-	pthread_mutex_lock(&(philo->monitor->m_print));
 	pthread_mutex_lock(&(philo->monitor->m_finish));
 	if (philo->monitor->finish_flag != 0)
 	{
 		pthread_mutex_unlock(&(philo->monitor->m_finish));
-		pthread_mutex_unlock(&(philo->monitor->m_print));
 		return (-1);
 	}
 	pthread_mutex_unlock(&(philo->monitor->m_finish));
@@ -103,10 +95,10 @@ int	print_think_state(t_philo *philo)
 		pthread_mutex_lock(&(philo->monitor->m_finish));
 		philo->monitor->finish_flag = 2;
 		pthread_mutex_unlock(&(philo->monitor->m_finish));
-		pthread_mutex_unlock(&(philo->monitor->m_print));
 		return (-1);
 	}
 	time_stamp = calc_timeval(&(philo->monitor->start_time), &(curr_time));
+	pthread_mutex_lock(&(philo->monitor->m_print));
 	printf("%ldms\t%d\tis thinking\n", time_stamp, philo->id);
 	pthread_mutex_unlock(&(philo->monitor->m_print));
 	return (0);
@@ -118,16 +110,15 @@ int	print_finish_state(t_philo *philo, int status)
 	long			time_stamp;
 	struct timeval	curr_time;
 
-	pthread_mutex_lock(&(philo->monitor->m_print));
 	if (gettimeofday(&(curr_time), NULL) != 0)
 	{
 		pthread_mutex_lock(&(philo->monitor->m_finish));
 		philo->monitor->finish_flag = 2;
 		pthread_mutex_unlock(&(philo->monitor->m_finish));
-		pthread_mutex_unlock(&(philo->monitor->m_print));
 		return (-1);
 	}
 	time_stamp = calc_timeval(&(philo->monitor->start_time), &(curr_time));
+	pthread_mutex_lock(&(philo->monitor->m_print));
 	if (status == DIE)
 		printf("%ldms\t%d\tis died\n", time_stamp, philo->id);
 	else if (status == FULL)
