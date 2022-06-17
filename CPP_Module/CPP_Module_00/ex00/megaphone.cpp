cb10d0ace23bc84a5a396a435a136ec4daee239f
Yejin Kim <yejikim@c8r6s10.42seoul.kr>
2022-06-17 15:20:35 +0900

    style: ex00 code style microsoft로 변경

--- CPP_Module/CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module/CPP_Module_00/ex00/megaphone.cpp
@@ -3,28 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
+/*   By: yejikim <yejikim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 12:07:37 by yejin             #+#    #+#             */
-/*   Updated: 2022/06/15 15:42:58 by yejin            ###   ########.fr       */
+/*   Updated: 2022/06/17 14:50:04 by yejikim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	if (argc < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	else {
-		std::string s;
-		for (int i=1; i<argc; i++) {
-			for (int j=0; argv[i][j] != '\0'; j++) {
-				s += toupper(argv[i][j]);
-			}
-		}
-		std::cout << s << '\n';
-	}
-	return (0);
+    if (argc < 2)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+    else
+    {
+        std::string s;
+        for (int i = 1; i < argc; i++)
+        {
+            for (int j = 0; argv[i][j] != '\0'; j++)
+            {
+                s += toupper(argv[i][j]);
+            }
+        }
+        std::cout << s << '\n';
+    }
+    return (0);
 }
