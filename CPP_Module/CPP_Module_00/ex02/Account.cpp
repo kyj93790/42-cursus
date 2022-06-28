6b214461e0bb6c1858db953b680811b02eb00565
kyj93790 <kyj93790@naver.com>
2022-06-28 18:16:02 +0900

    feat: timestamp 출력 구현(strftime)

--- CPP_Module/CPP_Module_00/ex02/Account.cpp
+++ CPP_Module/CPP_Module_00/ex02/Account.cpp
@@ -3,18 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yejikim <yejikim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 20:17:52 by yejikim           #+#    #+#             */
-/*   Updated: 2022/06/27 21:36:34 by yejikim          ###   ########.fr       */
+/*   Updated: 2022/06/28 18:15:32 by yejin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <ctime>
+#include <iostream>
 
 static void printTime()
 {
-	
+	std::time_t t = std::time(NULL);
+	if (t == (time_t)(-1))
+	{
+		std::cout << "ERROR : time()\n";
+		exit(EXIT_FAILURE);
+	}
+	char timestamp[16];
+	if (std::strftime(timestamp, sizeof(timestamp), "%Y%m%d_%H%M%S", localtime(&t)) == 0) {
+		std::cout << "ERROR : time()\n";
+		exit(EXIT_FAILURE);
+	}
+	std::cout << '[' << timestamp << ']' << ' ';
 }
 
 Account::Account()
@@ -23,5 +36,6 @@ Account::Account()
 
 Account::Account(int initial_deposit)
 {
+	printTime();
 	
 }
