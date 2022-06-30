4cea3378020e9ab37b4f824395b70ff7fcdf184f
kyj93790 <kyj93790@naver.com>
2022-06-30 13:31:44 +0900

    [cpp02 ex02] feat: 연산자 오버로딩(comparison, arithmetic, prefix, postfix, min, max)

---
+++ CPP_Module/CPP_Module_02/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 21:38:33 by yejin             #+#    #+#             */
+/*   Updated: 2022/06/30 13:30:24 by yejin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main(void)
+{
+	Fixed		a;
+	Fixed const	b( Fixed( 5.05f) * Fixed( 2 ) );
+
+	std::cout <<  a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max(a, b) << std::endl;
+
+	return (0);
+}
