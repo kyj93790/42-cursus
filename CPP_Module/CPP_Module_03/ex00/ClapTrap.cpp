e6af8989d4cf353920afdc3e061d59af001bad90
kyj93790 <kyj93790@naver.com>
2022-06-30 23:56:00 +0900

    [cpp03 ex00] be repaired 실행 불가 조건 추가

--- CPP_Module/CPP_Module_03/ex00/ClapTrap.cpp
+++ CPP_Module/CPP_Module_03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 23:00:11 by yejin             #+#    #+#             */
-/*   Updated: 2022/06/30 23:53:25 by yejin            ###   ########.fr       */
+/*   Updated: 2022/06/30 23:55:16 by yejin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,9 @@ void ClapTrap::takeDamage(unsigned int amount)
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	if (_energyPoints == 0) {
+	if (_hitPoints == 0)
+		std::cout << "ClapTrap can't take damage : discharged\n";
+	else if (_energyPoints == 0) {
 		std::cout << "ClapTrap can't be repaired : lack of energy\n";
 	}
 	else {
