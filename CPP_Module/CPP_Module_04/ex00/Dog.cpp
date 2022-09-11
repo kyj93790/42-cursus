8964cd13512d3d95555ade52b5d338082ac468e2
kyj93790 <kyj93790@naver.com>
2022-09-11 16:15:29 +0900

    [cpp04 ex00] feat: Dog class

---
+++ CPP_Module/CPP_Module_04/ex00/Dog.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/11 15:42:15 by yejin             #+#    #+#             */
+/*   Updated: 2022/09/11 16:14:55 by yejin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+#include <iostream>
+
+Dog::Dog() : Animal("Dog") {
+	std::cout << "[Dog] Default constructor called\n";
+}
+
+Dog::Dog(const Dog& dog) {
+	std::cout << "[Dog] Copy constructor called\n";
+	*this = dog;
+}
+
+Dog::~Dog() {
+	std::cout << "[Dog] Destructor called\n";
+}
+
+const Dog& Dog::operator=(const Dog& dog) {
+	type = dog.type;
+	return (*this);
+}
---
+++ CPP_Module/CPP_Module_04/ex00/Dog.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/11 15:42:52 by yejin             #+#    #+#             */
+/*   Updated: 2022/09/11 16:14:56 by yejin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+# define DOG_HPP
+
+# include "Animal.hpp"
+
+class Dog : public Animal {
+	public:
+		Dog();
+		Dog(const Dog& dog);
+		~Dog();
+		const Dog& operator=(const Dog& dog);
+};
+
+#endif
