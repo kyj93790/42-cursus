9cc9e2ca5890dd4fd67901ddd1366450ddbdafdb
kyj93790 <kyj93790@naver.com>
2022-09-11 16:43:33 +0900

    [cpp04 ex00] chore: main 함수 출력 형식 수정

--- CPP_Module/CPP_Module_04/ex00/main.cpp
+++ CPP_Module/CPP_Module_04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 15:41:24 by yejin             #+#    #+#             */
-/*   Updated: 2022/09/11 16:37:17 by yejin            ###   ########.fr       */
+/*   Updated: 2022/09/11 16:41:56 by yejin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,19 @@
 #include <iostream>
 
 int main(void) {
+	char temp[10];
+	
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+	std::cout << '\n';
+	std::cin.getline(temp, 1);
 	
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
+	std::cout << '\n';
+	std::cin.getline(temp, 1);
+	
 	i->makeSound();
 	j->makeSound();
 	meta->makeSound();
