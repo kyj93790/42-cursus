20161849f803b8ba1358684b3f14d50481be5b21
kyj93790 <kyj93790@naver.com>
2022-09-11 16:23:26 +0900

    [cpp04 ex00] Cat class

---
+++ CPP_Module/CPP_Module_04/ex00/Cat.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/11 15:42:15 by yejin             #+#    #+#             */
+/*   Updated: 2022/09/11 16:23:01 by yejin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+#include <iostream>
+
+Cat::Cat() : Animal("Cat") {
+	std::cout << "[Cat] Default constructor called\n";
+}
+
+Cat::Cat(const Cat& cat) {
+	std::cout << "[Cat] Copy constructor called\n";
+	*this = cat;
+}
+
+Cat::~Cat() {
+	std::cout << "[Cat] Destructor called\n";
+}
+
+const Cat& Cat::operator=(const Cat& cat) {
+	type = cat.type;
+	return (*this);
+}
---
+++ CPP_Module/CPP_Module_04/ex00/Cat.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yejin <yejin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/11 15:42:52 by yejin             #+#    #+#             */
+/*   Updated: 2022/09/11 16:22:12 by yejin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CAT_HPP
+# define CAT_HPP
+
+# include "Animal.hpp"
+
+class Cat : public Animal {
+	public:
+		Cat();
+		Cat(const Cat& cat);
+		~Cat();
+		const Cat& operator=(const Cat& cat);
+};
+
+#endif
