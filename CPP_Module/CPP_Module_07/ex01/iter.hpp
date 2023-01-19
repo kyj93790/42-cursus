ef63637b2151f8884a174fe03d6609edf3b87cc7
kyj93790 <kyj93790@naver.com>
2023-01-19 14:32:38 +0900

    [cpp07 ex01] const type iter 추가

--- CPP_Module/CPP_Module_07/ex01/iter.hpp
+++ CPP_Module/CPP_Module_07/ex01/iter.hpp
@@ -15,6 +15,13 @@ void print(T &a) {
 }
 
 template<typename T>
+void iter(const T *arr, size_t len, void (*f)(const T &t)) {
+	for (size_t i=0; i<len; i++) {
+		f(arr[i]);
+	}
+}
+
+template<typename T>
 void iter(T *arr, size_t len, void (*f)(T &t)) {
 	for (size_t i=0; i<len; i++) {
 		f(arr[i]);
