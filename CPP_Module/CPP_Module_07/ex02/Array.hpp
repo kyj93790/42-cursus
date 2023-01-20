a3463f7a08ca625e602fcb3743051f2404006368
kyj93790 <kyj93790@naver.com>
2023-01-20 09:11:31 +0900

    [cpp07 ex02] feat: Array template

---
+++ CPP_Module/CPP_Module_07/ex02/Array.hpp
@@ -0,0 +1,44 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <exception>
+# include <iostream>
+
+template <typename T>
+class Array {
+	private:
+		int _size;
+		T *_arr;
+	public:
+		Array() : _size(0), _arr(new T[0]) { };
+		Array(unsigned int n) : _size(n), _arr(new T[n]) { };
+		Array(const Array& arr) {
+			_size = arr._size;
+			if (this != &arr)
+				_arr = new T[arr.size()];
+		};
+		~Array() {
+			if (_arr) delete _arr;
+		};
+		Array& operator=(const Array& arr) {
+			_size = arr._size;
+			if (this != &arr) {
+				if (_arr) delete _arr;
+				_arr = new T[arr.size()];
+			}
+			return (*this);
+		};
+		T& operator[](int index) {
+			if (index < 0 || index >= size()) throw std::out_of_range("Error : out of range");
+			return _arr[index];
+		};
+		const T& operator[](int index) const {
+			if (index < 0 || index >= size()) throw std::out_of_range("Error : out of range");
+			return _arr[index];
+		};
+		int size() const {
+			return _size;
+		};
+};
+
+#endif
