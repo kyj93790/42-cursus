01ba3ea120895c9e1fa4d943b0cc30aed2790b37
Yejin Kim <yejikim@c8r6s10.42seoul.kr>
2023-01-24 11:49:59 +0900

    [cpp08 ex02] iterator reverse, const 구현 추가

--- CPP_Module/CPP_Module_08/ex02/MutantStack.hpp
+++ CPP_Module/CPP_Module_08/ex02/MutantStack.hpp
@@ -7,12 +7,33 @@ template <typename T>
 class MutantStack : public std::stack<T> {
 	public:
 		typedef typename std::stack<T>::container_type::iterator iterator; // iterator of deque
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
 		iterator begin() {
 			return std::stack<T>::c.begin();
 		}
 		iterator end() {
 			return std::stack<T>::c.end();
+		}
+		const_iterator begin() const {
+			return std::stack<T>::c.begin();
+		}
+		const_iterator end() const {
+			return std::stack<T>::c.end();
+		}
+		reverse_iterator rbegin() {
+			return std::stack<T>::c.rbegin();
+		}
+		reverse_iterator rend() {
+			return std::stack<T>::c.rend();
+		}
+		const_reverse_iterator rbegin() const {
+			return std::stack<T>::c.rbegin();
+		}
+		const_reverse_iterator rend() const {
+			return std::stack<T>::c.rend();
 		}
 };
 
--- CPP_Module/CPP_Module_08/ex02/main.cpp
+++ CPP_Module/CPP_Module_08/ex02/main.cpp
@@ -7,19 +7,19 @@ int main(void) {
 	std::cout << "-----MutantStack-----\n";
 	MutantStack<int> mstack;
 
-	mstack.push(5);
-	mstack.push(17);
+	mstack.push(1);
+	mstack.push(3);
 
-	std::cout << mstack.top() << std::endl;
+	std::cout << "top : " << mstack.top() << std::endl;
 
 	mstack.pop();
 
-	std::cout << mstack.size() << std::endl;
+	std::cout << "size : " << mstack.size() << std::endl;
 
 	mstack.push(3);
-	mstack.push(5);
 	mstack.push(737);
-	//[...]
+	mstack.push(-9);
+	mstack.push(100);
 	mstack.push(0);
 
 	MutantStack<int>::iterator it = mstack.begin();
@@ -32,25 +32,39 @@ int main(void) {
 		std::cout << *it << std::endl;
 		++it;
 	}
+
+	std::cout << "--MutantStack Reverse--\n";
+
 	std::stack<int> s(mstack);
+
+	MutantStack<int>::reverse_iterator r_it = mstack.rbegin();
+	MutantStack<int>::reverse_iterator r_ite = mstack.rend();
+
+	++r_it;
+	--r_it;
+	while (r_it != r_ite)
+	{
+		std::cout << *r_it << std::endl;
+		++r_it;
+	}
 
 	std::cout << "--------LIST-------\n";
 
 	std::list<int> lstack;
 
-	lstack.push_back(5);
-	lstack.push_back(17);
+	lstack.push_back(1);
+	lstack.push_back(3);
 
-	std::cout << lstack.back() << std::endl;
+	std::cout << "back : " << lstack.back() << std::endl;
 
 	lstack.pop_back();
 
-	std::cout << lstack.size() << std::endl;
+	std::cout << "size : " << lstack.size() << std::endl;
 
 	lstack.push_back(3);
-	lstack.push_back(5);
 	lstack.push_back(737);
-	//[...]
+	lstack.push_back(-9);
+	lstack.push_back(100);
 	lstack.push_back(0);
 
 	std::list<int>::iterator it_l = lstack.begin();
@@ -63,6 +77,20 @@ int main(void) {
 		std::cout << *it_l << std::endl;
 		++it_l;
 	}
+
+	std::cout << "-----LIST Reverse----\n";
+
 	std::list<int> s_l(lstack);
+
+	std::list<int>::reverse_iterator r_it_l = lstack.rbegin();
+	std::list<int>::reverse_iterator r_ite_l = lstack.rend();
+	
+	++r_it_l;
+	--r_it_l;
+	while (r_it_l != r_ite_l)
+	{
+		std::cout << *r_it_l << std::endl;
+		++r_it_l;
+	}
 	return 0;
 }
